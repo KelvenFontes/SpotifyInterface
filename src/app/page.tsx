3e16cd3314d2ed4b4d1c53b690db2b6d8cc2fa3c
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-05 20:24:56 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react";
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Laptop2, Mic2, LayoutList, Volume, Maximize2 } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col bg-black">
+    <div className="h-screen flex flex-col bg-black overflow-hidden">
 
       <div className="flex flex-1 pl-2 pr-2 gap-2 m-2">
 
@@ -15,7 +15,7 @@ export default function Home() {
             <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-200 hover:text-zinc-100"><Library />Your Library</a>
           </nav>
 
-          <nav className="mt-2 p-4 bg-zinc-900 flex flex-col gap-3 rounded-lg h-[450px]">
+          <nav className="mt-2 p-4 bg-zinc-900 flex flex-col gap-3 rounded-lg h-[426px]">
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Aniversário</a>
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Hot Hits Brasil</a>
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Top Brasil</a>
@@ -25,7 +25,7 @@ export default function Home() {
 
         </aside>
 
-        <main className="flex-1 bg-zinc-900 p-6 rounded-lg h-[600px] overflow-y-auto appearance-none">
+        <main className="flex-1 bg-zinc-900 p-6 rounded-lg h-[578px] overflow-y-auto appearance-none">
           <div className="flex items-center gap-2">
             <button className="p-1 rounded-full bg-black/40"><ChevronLeft /></button>
             <button className="p-1 rounded-full bg-black/40"><ChevronRight /></button>
@@ -35,32 +35,32 @@ export default function Home() {
 
           <div className="grid grid-cols-3 gap-4 mt-4">
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album1.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album1.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album1.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album1.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album2.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album2.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album2.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album2.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album3.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album3.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
             <a href="" className="group bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-              <Image src="/album3.png" width={76} height={76} alt="Capa do album" />
+              <Image src="/album3.png" width={72} height={72} alt="Capa do album" />
               <strong>Wasting Light</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-[#1abc54] text-black ml-auto mr-4 invisible group-hover:visible"><Play /></button>
             </a>
@@ -99,10 +99,49 @@ export default function Home() {
         </main>
       </div >
 
-      <footer className="p-6 bg-zinc-950 flex items-center justify-between">
-        <div></div>
-        <div></div>
-        <div></div>
+      <footer className="p-4 bg-zinc-950 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/album3.png" className="w-full rounded-lg" width={48} height={48} alt="Capa do album" />
+          <div className="flex flex-col">
+            <strong className="font-normal">Rope</strong>
+            <span className="text-xs text-zinc-400">Foo Fighthervfvdsfvfsdvdsvdfsvsvvd</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-4">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black"><Play /></button>
+
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-500">0:42</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-500">3:22</span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <LayoutList size={20} />
+          <Mic2 size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="h-1 rounded-full w-10 bg-zinc-200"></div>
+            </div>
+
+          </div>
+          <Maximize2 size={20} />
+        </div>
+
       </footer>
     </div >
   )
